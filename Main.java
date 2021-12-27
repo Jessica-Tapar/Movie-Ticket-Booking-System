9be255cdadbb2166af9b8f5623d47d9418a46a5b
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2021-12-27 15:59:04 +0530

    Updated

--- Main.java
+++ Main.java
@@ -4,10 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner st= new Scanner(System.in);
-        Booking_System obj= new Booking_System();
-        obj.bookticket();
-        obj.vacancies();
-        obj.bookticket();
-        obj.vacancies();
+
+        Movies M = new Movies();
+
+        System.out.println("------------------------------- # Welcome to B3 Theatre # -------------------------------");
+
+
+        while(true) {
+            System.out.print("Choose option: \n1. BOOK TICKETS\n2. CANCEL TICKETS\n3. DISPLAY VACANCIES\n4. EXIT\nEnter: ");
+            char option = st.next().charAt(0);
+            System.out.println();
+
+            if(option=='1') M.choose_movie();
+            else if(option=='2') M.cancellation();
+            else if(option=='4') break;
+            else if(option=='3') M.display();
+            else System.out.println("Invalid Option!");
+        }
     }
 }
